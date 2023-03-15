1039e5e817b3feadae3e000412d6127c9b3bd64a
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-03-15 13:53:04 +0300

    Update Profile Screen paddings

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -13,41 +13,53 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<ProfileCubit, ProfileState>(builder: (context, state) {
-        return state.maybeMap(
-          success: (usrState) => ListView(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(top: 24),
-                alignment: Alignment.center,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 16),
-                      width: 140,
-                      height: 140,
-                      child: CircleAvatar(
-                        radius: 50,
-                        child: ClipOval(
-                          child: usrState.avatar,
+      body: SafeArea(
+        child:
+            BlocBuilder<ProfileCubit, ProfileState>(builder: (context, state) {
+          return state.maybeMap(
+            success: (usrState) => Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(top: 24),
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 16),
+                          width: 140,
+                          height: 140,
+                          child: CircleAvatar(
+                            radius: 50,
+                            child: ClipOval(
+                              child: usrState.avatar,
+                            ),
+                          ),
                         ),
-                      ),
+                        CustomText.headlineSmall(
+                          usrState.user.name,
+                          fontWeight: 700,
+                          letterSpacing: 0,
+                          height: 0.8,
+                          color: Theme.of(context).primaryColor,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        CustomText.titleSmall(
+                          usrState.prefs.data['academicGroup'] ??
+                              "неизвестная группа",
+                          fontWeight: 600,
+                        ),
+                      ],
                     ),
-                    CustomText.headlineSmall(usrState.user.name,
-                        fontWeight: 700, letterSpacing: 0),
-                    CustomText.titleSmall(
-                      usrState.prefs.data['academicGroup'] ??
-                          "неизвестная группа",
-                      fontWeight: 600,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 20),
-                child: Container(
-                  margin: const EdgeInsets.only(left: 24, right: 24, top: 24),
-                  child: Column(
+                  ),
+                  const SizedBox(
+                    height: 44,
+                  ),
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       const CustomText.titleMedium(
@@ -61,32 +73,29 @@ class ProfileScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 24, right: 24),
-                child: CustomButton.rounded(
-                  elevation: 0,
-                  onPressed: () {
-                    context.read<ProfileCubit>().logout();
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => const LoginHomeScreen()));
-                  },
-                  child: const CustomText.titleSmall(
-                    "Выйти",
-                    color: Colors.white,
-                    fontWeight: 700,
+                  const SizedBox(
+                    height: 24,
                   ),
-                ),
+                  CustomButton.rounded(
+                    elevation: 0,
+                    onPressed: () {
+                      context.read<ProfileCubit>().logout();
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const LoginHomeScreen()));
+                    },
+                    child: const CustomText.titleSmall(
+                      "Выйти",
+                      color: Colors.white,
+                      fontWeight: 700,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-          orElse: () => const Center(child: CircularProgressIndicator()),
-        );
-      }),
+            ),
+            orElse: () => const Center(child: CircularProgressIndicator()),
+          );
+        }),
+      ),
     );
   }
 }
