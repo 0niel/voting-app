bb2358f4e7833858262351383d4431b181e1d384
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-03-15 15:03:16 +0300

    Refactor poll screen

--- lib/views/poll/poll_screen.dart
+++ lib/views/poll/poll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:appwrite/models.dart';
 import 'package:collection/collection.dart';
 import 'package:face_to_face_voting/blocs/events/events_cubit.dart';
 import 'package:face_to_face_voting/blocs/poll/poll_cubit.dart';
@@ -6,6 +7,7 @@ import 'package:face_to_face_voting/theme/app_theme.dart';
 import 'package:face_to_face_voting/utils/spacing.dart';
 import 'package:face_to_face_voting/widgets/text.dart';
 import 'package:face_to_face_voting/widgets/votes_banner.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,12 +19,45 @@ class PollScreen extends StatefulWidget {
 }
 
 class _PollScreenState extends State<PollScreen> {
+  bool _isFirstBuild = true;
+
   @override
   void initState() {
     super.initState();
   }
 
-  int? _selectedOption = -1;
+  final ValueNotifier<int?> _selectedOption = ValueNotifier<int?>(-1);
+
+  void _setMyVote(String meId, DocumentList votes, List<String> options) {
+    if (!_isFirstBuild) {
+      return;
+    }
+
+    final String? myVote;
+    final myVoteDoc =
+        votes.documents.firstWhereOrNull((doc) => doc.data['voter_id'] == meId);
+    if (myVoteDoc != null) {
+      myVote = myVoteDoc.data['vote'] as String;
+    } else {
+      myVote = null;
+    }
+
+    final index = options.indexWhere((element) => element == myVote);
+    if (index != -1 && index != _selectedOption.value) {
+      _selectedOption.value = index;
+    }
+
+    _isFirstBuild = false;
+  }
+
+  Map<String, int> _getVotes(DocumentList votes) {
+    final votesMap = votes.documents.fold<Map<String, int>>({}, (map, doc) {
+      final vote = doc.data['vote'] as String;
+      return map..update(vote, (count) => count + 1, ifAbsent: () => 1);
+    });
+
+    return votesMap;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +67,8 @@ class _PollScreenState extends State<PollScreen> {
           return profileState.maybeMap(
             orElse: () => Container(),
             success: (user) {
+              final me = user.user;
+
               return BlocConsumer<EventsCubit, EventsState>(
                 listener: (context, state) {
                   state.maybeWhen(
@@ -55,35 +92,20 @@ class _PollScreenState extends State<PollScreen> {
                             final options =
                                 List<String>.from(poll.data['poll_options']);
 
-                            final votesMap = votes.documents
-                                .fold<Map<String, int>>({}, (map, doc) {
-                              final vote = doc.data['vote'] as String;
-                              return map
-                                ..update(vote, (count) => count + 1,
-                                    ifAbsent: () => 1);
-                            });
+                            _setMyVote(me.$id, votes, options);
 
-                            final meId = user.user.$id;
-
-                            final String? myVote;
-                            final myVoteDoc = votes.documents.firstWhereOrNull(
-                                (doc) => doc.data['voter_id'] == meId);
-                            if (myVoteDoc != null) {
-                              myVote = myVoteDoc.data['vote'] as String;
-                            } else {
-                              myVote = null;
-                            }
-
-                            return _buildPoll(
-                              eventName,
-                              question,
-                              options,
-                              votesMap,
-                              (timeLeft.isNegative || timeLeft.inSeconds == 0)
+                            return _Poll(
+                              eventName: eventName,
+                              question: question,
+                              options: options,
+                              votes: _getVotes(votes),
+                              isActive: (timeLeft.isNegative ||
+                                      timeLeft.inSeconds == 0)
                                   ? false
                                   : true,
-                              eventId,
-                              poll.$id,
+                              eventId: eventId,
+                              pollId: poll.$id,
+                              selectedOptionNotifier: _selectedOption,
                             );
                           },
                           noPoll: (eventId) =>
@@ -107,15 +129,98 @@ class _PollScreenState extends State<PollScreen> {
       ),
     );
   }
+}
 
-  Widget _buildPoll(String eventName, String question, List<String> options,
-      Map<String, int> votes, bool isActive, String eventId, String pollId) {
+class _PollAnswerOption extends StatelessWidget {
+  const _PollAnswerOption({
+    Key? key,
+    this.index,
+    required this.option,
+    required this.onTap,
+    required this.selectedOptionNotifier,
+  }) : super(key: key);
+
+  final int? index;
+  final String option;
+  final VoidCallback onTap;
+
+  final ValueNotifier<int?> selectedOptionNotifier;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (index != selectedOptionNotifier.value) {
+          onTap();
+          selectedOptionNotifier.value = index;
+        }
+      },
+      child: ValueListenableBuilder(
+        valueListenable: selectedOptionNotifier,
+        builder: (context, value, child) {
+          return Container(
+            decoration: BoxDecoration(
+              color: selectedOptionNotifier.value == index
+                  ? AppTheme.theme.colorScheme.primary
+                  : Colors.transparent,
+              border: Border.all(
+                  color: selectedOptionNotifier.value == index
+                      ? AppTheme.theme.colorScheme
+                          .primary //_getColorBySelectedOption(option)
+                      : AppTheme.theme.colorScheme.onBackground),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(4),
+              ),
+            ),
+            padding: Spacing.fromLTRB(0, 12, 0, 12),
+            margin: Spacing.fromLTRB(48, 0, 48, 16),
+            child: Center(
+              child: CustomText.bodyLarge(
+                option,
+                color: selectedOptionNotifier.value == index
+                    ? AppTheme.theme.colorScheme.onPrimary
+                    : AppTheme.theme.colorScheme.onBackground,
+                fontWeight: 600,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _Poll extends StatelessWidget {
+  const _Poll({
+    Key? key,
+    required this.eventName,
+    required this.question,
+    required this.options,
+    required this.votes,
+    required this.isActive,
+    required this.eventId,
+    required this.pollId,
+    required this.selectedOptionNotifier,
+  }) : super(key: key);
+
+  final String eventName;
+  final String question;
+  final List<String> options;
+  final Map<String, int> votes;
+  final bool isActive;
+  final String eventId;
+  final String pollId;
+
+  final ValueNotifier<int?> selectedOptionNotifier;
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
+      children: [
         Container(
           padding: Spacing.fromLTRB(16, 42, 16, 0),
           child: Column(
-            children: <Widget>[
+            children: [
               CustomText.titleMedium(eventName, //value.event.data['name'],
                   color: AppTheme.theme.colorScheme.onBackground,
                   fontWeight: 700),
@@ -132,7 +237,7 @@ class _PollScreenState extends State<PollScreen> {
             padding: Spacing.fromLTRB(48, 0, 48, 0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
+              children: [
                 CustomText.titleLarge(
                   question,
                   letterSpacing: 0.2,
@@ -148,7 +253,7 @@ class _PollScreenState extends State<PollScreen> {
                         .asMap()
                         .entries
                         .map(
-                          (e) => questionOption(
+                          (e) => _PollAnswerOption(
                             option: e.value,
                             index: e.key,
                             onTap: () {
@@ -157,6 +262,7 @@ class _PollScreenState extends State<PollScreen> {
                                     .sendVote(eventId, pollId, e.value);
                               }
                             },
+                            selectedOptionNotifier: selectedOptionNotifier,
                           ),
                         )
                         .toList(),
@@ -170,7 +276,8 @@ class _PollScreenState extends State<PollScreen> {
         Container(
           padding: Spacing.fromLTRB(48, 0, 48, 0),
           child: CustomText.bodySmall(
-            "Голосование завершится автоматически, когда все участники проголосуют. Если вы не успеете завершить голосование, ваш голос автоматически отправится как \"воздержусь\".",
+            "Голосование завершится автоматически, когда все участники проголосуют."
+            "${options.indexWhere((element) => element.toLowerCase() == 'воздержусь') == -1 ? "" : " Если вы не успеете завершить голосование, ваш голос автоматически отправится как \"воздержусь\"."}",
             color: AppTheme.theme.colorScheme.onBackground,
             muted: true,
             textAlign: TextAlign.justify,
@@ -178,43 +285,6 @@ class _PollScreenState extends State<PollScreen> {
         ),
         const SizedBox(height: 25),
       ],
-    );
-  }
-
-  Widget questionOption({
-    required String option,
-    int? index,
-    required Function onTap,
-  }) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          if (index != _selectedOption) onTap();
-          _selectedOption = index;
-        });
-      },
-      child: Container(
-        decoration: BoxDecoration(
-            color: _selectedOption == index
-                ? AppTheme.theme.colorScheme
-                    .primary //_getColorBySelectedOption(option)
-                : Colors.transparent,
-            border: Border.all(
-                color: _selectedOption == index
-                    ? AppTheme.theme.colorScheme
-                        .primary //_getColorBySelectedOption(option)
-                    : AppTheme.theme.colorScheme.onBackground),
-            borderRadius: const BorderRadius.all(Radius.circular(4))),
-        padding: Spacing.fromLTRB(0, 12, 0, 12),
-        margin: Spacing.fromLTRB(48, 0, 48, 16),
-        child: Center(
-          child: CustomText.bodyLarge(option,
-              color: _selectedOption == index
-                  ? AppTheme.theme.colorScheme.onPrimary
-                  : AppTheme.theme.colorScheme.onBackground,
-              fontWeight: 600),
-        ),
-      ),
     );
   }
 }
