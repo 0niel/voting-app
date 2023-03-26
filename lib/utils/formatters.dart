c59757dc5ee2d0fe602167cd2252bc722f851278
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-03-26 19:54:51 +0300

    Fix date formatter timezone

--- lib/utils/formatters.dart
+++ lib/utils/formatters.dart
@@ -2,7 +2,9 @@ class StringFormatter {
   static String formatDateTime(String? dateTimeString) {
     if (dateTimeString == null) return "не указана";
 
-    final dateTime = DateTime.parse(dateTimeString);
+    final dateTime =
+        DateTime.parse(dateTimeString).add(const Duration(hours: 3));
+
     return "${dateTime.day.toString().padLeft(2, '0')}.${dateTime.month.toString().padLeft(2, '0')}.${dateTime.year} ${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}";
   }
 
